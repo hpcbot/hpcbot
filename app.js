5137f6254de45b5afc417ae49b69b3f154942a04
bdickason <dickason@gmail.com>
2017-03-22 01:19:19 -0700

    Can test the \!sortinghat command

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ require('dotenv').config();	// Load environment variables from .env
 
 var tmi = require("tmi.js");
 var config = require("./config/tmi-options.js");
+var chat = require('./lib/chat.js');
 
 var client = new tmi.client(config.options);
+var channel = config.options.channels[0];	// We'll need to reference the active channel
 
 client.connect();
+
+client.on('chat', function (channel, userstate, message, self) {
+	chat.command(userstate, message, function(response) {
+		if(response) {
+			client.say(channel, response);	
+		}
+	});
+});
+
