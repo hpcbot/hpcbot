0cccb5d28497604767cfe07141bc400c1cc14a61
bdickason <dickason@gmail.com>
2017-03-23 23:58:08 -0700

    Can add users to the database

--- test/test-user.js
+++ test/test-user.js
@@ -18,7 +18,7 @@ describe('Users', function() {
 		});
 	});
 	
-	it('Gets a list of all users', function(done) {
+	it('all: Gets a list of all users', function(done) {
 		var _users = ['bdickason'];
 
 		// Find the next available unique identifier
@@ -38,7 +38,7 @@ describe('Users', function() {
 		});		
 	});
 
-	it('Gets a list of all user ids', function(done) {
+	it('ids: Gets a list of all user ids', function(done) {
 		var _users = ['1'];
 
 		// Find the next available unique identifier
@@ -58,7 +58,7 @@ describe('Users', function() {
 		});		
 	});
 
-	it('Gets an individual user id from username', function(done) {
+	it('id: Gets an individual user id from username', function(done) {
 		var _users = ['5'];
 		// Find an arbitrary unique identifier that is not 1
 		db.get().incrby('user:uids', 5, function(err, num) {
@@ -76,10 +76,37 @@ describe('Users', function() {
 			}
 		});		
 	});
+
+	it('add: Doesn\'t create a new user if one exists', function(done) {
+		var _err = "User already exists"
+		var _users = { uid: '1' };
+
+		// Find the next available unique identifier
+		db.get().incrby('user:uids', 1, function(err, num) {
+			if(!err) {
+				// add user to users hash
+				db.get().hset('user:bdickason', 'uid', num, function(err, data) {
+					// add userto list of usernames
+					db.get().hset('users', 'bdickason', num, function(err, data) {
+						Users.add('bdickason', function(err, users) {
+							assert.equal(_err, err);
+							assert.deepEqual(_users, users);
+							done();
+						});
+					});
+				});
+			}
+		});		
+	});
+
+	it('add: Creates a new user if one doesn\'t exist', function(done) {
+		var _err = null;
+		var _users = 1;
+
+		Users.add('bdickason', function(err, users) {	// I wish the other tests were this easy ;()
+			assert.equal(_err, err);
+			assert.deepEqual(_users, users);
+			done();
+		});
+	});
 });
-					// Get individual user:
-					// db.get().hget('user:' + num, 'username', function(err, data) {
-						// assert.equal(_numUsers, num);
-						// assert.equal(_username, data);
-						// done();
-					// });
