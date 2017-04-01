4a0465edea3f8f9a437d79ce04172ae42cd86967
bdickason <dickason@gmail.com>
2017-03-31 18:13:43 -0700

    Adjusted twitch event name to follow the pattern of 'destination:action'

--- app.js
+++ app.js
@@ -20,45 +20,6 @@ db.connect();
 var User = require ('./lib/user');
 var user = User.start(db);
 
-});
-
-
-// var strings = require('./config/strings.json');
-
-// var Chat = require('./lib/chat');
-// var Users = require('./lib/user');
-// var db = require('./lib/db');
-
-
-// var client = new tmi.client(config.options);
-// var channel = config.options.channels[0];	// We'll need to reference the active channel
-
-// db.connect();
-/*
-client.connect();
-
-/* User said something in the channel */
-// client.on('chat', function (channel, userstate, message, self) {
-	// Check if chat line was a command
-	// Chat.command(userstate, message, self, function(response) {
-		// if(response) {
-			// response.forEach(function(line) {
-				// Handle multi-line responses
-				// client.say(channel, line);
-			// });
-		// }
-	// });
-// });
-
-/* User sent a private message */
-// client.on('whisper', function (from, userstate, message, self) {
-	// Check if message was a command
-	// Chat.command(userstate, message, self, function(response) {
-		// if(response) {
-			// client.whisper(from, response);	
-		// }
-	// });
-// });
 
 // client.on('join', function (channel, userstate, self) {
 	// var username = userstate;	// For some reason in this context, the userstate has no additional info
