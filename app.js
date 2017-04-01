55ae7623ce6adcd2403d68307b7c400986aab4d5
Brad Dickason <dickason@gmail.com>
2017-03-31 18:36:26 -0700

    Rewrote bot to use a pub/sub architecture to fix deep coupling of functions (#20)
    * Added basic eventbus and refactored twitch to live in its own module
    
    * Twitch chat gets passed to the chat module
    
    * Chat now parses properly
    
    * Added standalone chat module with proper unit tests
    
    * Rules work!
    
    * Command parser is functioning with full test coverage
    
    * Added more test cases for rules
    
    * Cleanup old comments
    
    * Added clarity to command test cases
    
    * database is now injected into start function
    
    * First passing user test
    
    * Added test library chai to verify array inclusion
    
    * Refactored new user and existing user tests
    
    * All tests pass
    
    * sort is fully tested
    
    * Full coverage for sorting hat
    
    * user commands now go to chat.js to generate strings
    
    * End to end sorting test works
    
    * fixed bug where tests would fail async
    
    * Added first test of house
    
    * Added test coverage for house
    
    * Tests now cover all Sorted cases
    
    * Added complete tests for house command
    
    * All test cases accounted for - ready to test\!
    
    * Adjusted twitch event name to follow the pattern of 'destination:action'
    
    * Removed channel from parameters when unnecessary
    
    * Bot now properly greets new users

--- app.js
+++ app.js
@@ -2,58 +2,38 @@
 
 require('dotenv').config();	// Load environment variables from .env
 
-var tmi = require("tmi.js");
+var eventbus = require('./lib/eventbus'); // Global event bus that modules can pub/sub to
 
-var config = require("./config/tmi-options.js");
-var strings = require('./config/strings.json');
+// Initialize Twitch connection
+var Twitch = require('./lib/twitch');
+var twitch = Twitch.start();
 
-var Chat = require('./lib/chat');
-var Users = require('./lib/user');
+var Chat = require ('./lib/chat');
+var chat = Chat.start();
+
+var Commands = require ('./lib/commands');
+var commands = Commands.start();
+
 var db = require('./lib/db');
-
-
-var client = new tmi.client(config.options);
-var channel = config.options.channels[0];	// We'll need to reference the active channel
-
 db.connect();
-client.connect();
 
-/* User said something in the channel */
-client.on('chat', function (channel, userstate, message, self) {
-	// Check if chat line was a command
-	Chat.command(userstate, message, self, function(response) {
-		if(response) {
-			response.forEach(function(line) {
-				// Handle multi-line responses
-				client.say(channel, line);
-			});
-		}
-	});
-});
+var User = require ('./lib/user');
+var user = User.start(db);
 
-/* User sent a private message */
-client.on('whisper', function (from, userstate, message, self) {
-	// Check if message was a command
-	Chat.command(userstate, message, self, function(response) {
-		if(response) {
-			client.whisper(from, response);	
-		}
-	});
-});
 
-client.on('join', function (channel, userstate, self) {
-	var username = userstate;	// For some reason in this context, the userstate has no additional info
+// client.on('join', function (channel, userstate, self) {
+	// var username = userstate;	// For some reason in this context, the userstate has no additional info
 
 	// Hack to rule out bots or brokenness
-	if(username != 'undefined' || username != 'hpc_dumbledore' || username != 'moobot') {
+	// if(username != 'undefined' || username != 'hpc_dumbledore' || username != 'moobot') {
 		
 		// Create a user if one doesn't exist
-		Users.add(username, function(err, username) {
-			if(!err) {
+		// Users.add(username, function(err, username) {
+			// if(!err) {
 				// This is a new user
-				var response = strings.join.welcome + username + strings.join.new_here;
-				client.say(channel, response);
-			}
-		});
-	}	
-});
+				// var response = strings.join.welcome + username + strings.join.new_here;
+				// client.say(channel, response);
+			// }
+		// });
+	// }	
+// });
