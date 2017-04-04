b9a0448d81f72ef29a0b0b5596bef811c4c09a87
bdickason <dickason@gmail.com>
2017-04-04 01:15:39 -0700

    Happy path tests work - need to add moderation checks

--- app.js
+++ app.js
@@ -20,20 +20,5 @@ db.connect();
 var User = require ('./lib/user');
 var user = User.start(db);
 
-
-// client.on('join', function (channel, userstate, self) {
-	// var username = userstate;	// For some reason in this context, the userstate has no additional info
-
-	// Hack to rule out bots or brokenness
-	// if(username != 'undefined' || username != 'hpc_dumbledore' || username != 'moobot') {
-		
-		// Create a user if one doesn't exist
-		// Users.add(username, function(err, username) {
-			// if(!err) {
-				// This is a new user
-				// var response = strings.join.welcome + username + strings.join.new_here;
-				// client.say(channel, response);
-			// }
-		// });
-	// }	
-// });
+var Channel = require ('./lib/channel');
+var channel = Channel.start(db);
