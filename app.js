bfcf6371a42d14b0c62f90e4e98a3a352e8aa15e
bdickason <dickason@gmail.com>
2017-04-04 23:02:43 -0700

    Some sample code to test twitch functions. Does not work currently

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ require('dotenv').config();	// Load environment variables from .env
 var eventbus = require('./lib/eventbus'); // Global event bus that modules can pub/sub to
 
 // Initialize Twitch connection
+var tmi = require("tmi.js");	// Initialize tmi.js here so we can inject it during testing
+var config = require(".lib/twitch/config/tmi-options.js");
+var twitchClient = new tmi.client(config.options);
+twitchClient.connect();
+
 var Twitch = require('./lib/twitch');
-var twitch = Twitch.start();
+var twitch = Twitch.start(twitchClient, config.options.channels[0]);
 
 var Chat = require ('./lib/chat');
 var chat = Chat.start();
