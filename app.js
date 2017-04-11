044b6e7e34672c7a2dcee01576831540d72cb3e3
Brad Dickason <dickason@gmail.com>
2017-04-10 20:14:18 -0700

    Command module (Resolves #23) (#25)
    * Happy path tests work - need to add moderation checks
    
    * Some sample code to test twitch functions. Does not work currently
    
    * Bot now allows users to store their commends
    
    * User can set and retrieve commends
    
    * Split out rules into its own command module
    
    * Fixed up app.js so the bot runs properly
    
    * Added proper test coverage for rules module
    
    * Need to DI eventbus so tests work properly
    
    * Migrated rules to DI eventbus
    
    * Added eventbus DI to user
    
    * Migrated commands module to DI
    
    * Migrated Twitch module to DI events
    
    * Final eventbus moved to DI resolves #23
    
    * Created new House command module
    
    * Added sortinghat chat tests
    
    * Migrated sorting into its own command module
    
    * Added sinon sandboxes to make testing work across multiple files
    
    * Join has full test coverage
    
    * User can get their commends
    
    * Added tests for commends get/set
    
    * Final set of old tests for users
    
    * Removed old commands.js file which is no longer used
    
    * Fixed bug where commends would call other items
    
    * Can set commends via whisper
    
    * Added tests for Twitch client

--- app.js
+++ app.js
@@ -5,35 +5,34 @@ require('dotenv').config();	// Load environment variables from .env
 var eventbus = require('./lib/eventbus'); // Global event bus that modules can pub/sub to
 
 // Initialize Twitch connection
+var tmi = require("tmi.js");	// Initialize tmi.js here so we can inject it during testing
+var config = require("./lib/twitch/config/tmi-options.js");
+var twitchClient = new tmi.client(config.options);
+twitchClient.connect();
+
 var Twitch = require('./lib/twitch');
-var twitch = Twitch.start();
+var twitch = Twitch.start(eventbus, twitchClient, config.options.channels[0]);
 
 var Chat = require ('./lib/chat');
-var chat = Chat.start();
+var chat = Chat.start(eventbus);
 
-var Commands = require ('./lib/commands');
-var commands = Commands.start();
-
+// Load Models
 var db = require('./lib/db');
 db.connect();
 
-var User = require ('./lib/user');
-var user = User.start(db);
+var User = require('./lib/models/user');
+var user = User.start(eventbus, db);
 
 
-// client.on('join', function (channel, userstate, self) {
-	// var username = userstate;	// For some reason in this context, the userstate has no additional info
+// Load Command Modules
+var Rules = require('./lib/commands/rules');
+var rules = Rules.start(eventbus);
 
-	// Hack to rule out bots or brokenness
-	// if(username != 'undefined' || username != 'hpc_dumbledore' || username != 'moobot') {
-		
-		// Create a user if one doesn't exist
-		// Users.add(username, function(err, username) {
-			// if(!err) {
-				// This is a new user
-				// var response = strings.join.welcome + username + strings.join.new_here;
-				// client.say(channel, response);
-			// }
-		// });
-	// }	
-// });
+var House = require('./lib/commands/house');
+var house = House.start(eventbus, db);
+
+var Join = require('./lib/commands/join');
+var join = Join.start(eventbus, db);
+
+var Commends = require('./lib/commands/commends');
+var commends = Commends.start(eventbus, db);
