cc226637f9f8a8a24945921a96952f165900828b
bdickason <dickason@gmail.com>
2017-04-14 23:31:54 -0700

    testclient now starts server, runs client side tests, stops server

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@ var gulp = require('gulp');
 
 var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
+var casper = require('gulp-casperjs-local').default;
 
 gulp.task('test', function() {
 	return gulp
 		.src(['lib/**/*.spec.js'])
 		.pipe(mocha());
+});
+
+gulp.task('testclient', ['server', 'casper'], function() {
+	process.exit(1);
+});
+
+gulp.task('casper', function() {
+	return gulp
+		.src(['lib/**/*.test.js'])
+		.pipe(casper());
 });
 
 gulp.task('app', function() {
@@ -19,6 +30,8 @@ gulp.task('app', function() {
 gulp.task('server', function() {
 	// Just start the server
 	nodemon({
-		script: 'lib/server/server.js'
+		script: 'lib/server/server.js',
+		ext: '.js',
+		ignore: ['*.spec.js', '*.test.js']
 	});
 });
