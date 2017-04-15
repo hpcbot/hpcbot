0f434e9d9830d361b12f3cd2c5a88da8f70ac94e
bdickason <dickason@gmail.com>
2017-04-14 23:11:27 -0700

    Added basic gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 
 var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+	return gulp
+		.src(['lib/**/*.spec.js'])
+		.pipe(mocha());
+});
 
 gulp.task('app', function() {
   // Start the app
