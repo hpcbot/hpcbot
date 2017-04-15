59f7c19c80035f233daf51108c8350b739dd1005
bdickason <dickason@gmail.com>
2017-04-14 23:57:27 -0700

    Finalized gulp configuration resolves #31

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,35 @@ var casper = require('gulp-casperjs-local').default;
 
 /* Tests */
 gulp.task('test', ['mocha'], function() {
+	// Run all tests
 	gulp.run('testclient');
 	// process.exit(1);
 });
 
 gulp.task('testclient', ['server', 'casper'], function() {
+	// Run client side tests once
 	process.exit(1);
 });
 
 gulp.task('watch', function() {
 	// Run server-side tests whenever a server-side test changes
-	gulp.watch(['lib/**/*.js', '!*.test.js', '!lib/server/static/*.js'], ['mocha']);
+	gulp.watch(['lib/**/*.js', '!lib/**/*.test.js', '!lib/server/static/*.js'], ['mocha']);
+});
+
+gulp.task('watchclient', function() {
+	// Run client-side tests whenever a client-side test changes
+	gulp.watch(['!lib/**/*.js', 'lib/**/*.test.js', 'lib/server/static/*.js'], ['server', 'casper']);
 });
 
 gulp.task('mocha', function() {
-	// Run server-side tests
+	// Run server-side tests once
 	return gulp
 		.src(['lib/**/*.spec.js'])
 		.pipe(mocha());
 });
 
 gulp.task('casper', function() {
-	// Run client-side tests
+	// Run client-side testrunner (needs server)
 	return gulp
 		.src(['lib/**/*.test.js'])
 		.pipe(casper());
