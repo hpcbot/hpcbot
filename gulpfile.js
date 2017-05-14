9f50392e483dc3e0b46cec25432f301b3c386208
Brad Dickason <dickason@gmail.com>
2017-05-14 12:19:00 -0700

    Moved overlay server code to separate module (#34) resolves #33
    * Broke out overlays to standalone function
    
    * Fixed a few tests
    
    * Added casper util module to package files
    
    * Added server dependency
    
    * Added proper reference to twitch overlay module
    
    * Removed server-side tests from this repo
    
    * Images weren't displaying properly

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,11 @@ var mocha = require('gulp-mocha');
 var casper = require('gulp-casperjs-local').default;
 
 /* Tests */
-gulp.task('test', ['mocha'], function() {
-	// Run all tests
-	gulp.run('testclient');
-	// process.exit(1);
-});
-
-gulp.task('testclient', ['server', 'casper'], function() {
-	// Run client side tests once
-	process.exit(0);
-});
+gulp.task('test', ['mocha']);
 
 gulp.task('watch', function() {
 	// Run server-side tests whenever a server-side test changes
-	gulp.watch(['lib/**/*.js', '!lib/**/*.test.js', '!lib/server/static/*.js'], ['mocha']);
-});
-
-gulp.task('watchclient', function() {
-	// Run client-side tests whenever a client-side test changes
-	gulp.watch(['!lib/**/*.js', 'lib/**/*.test.js', 'lib/server/static/*.js'], ['server', 'casper']);
+	gulp.watch(['lib/**/*.js', '!lib/**/*.test.js'], ['mocha']);
 });
 
 gulp.task('mocha', function() {
@@ -33,28 +19,10 @@ gulp.task('mocha', function() {
 		.pipe(mocha());
 });
 
-gulp.task('casper', function() {
-	// Run client-side testrunner (needs server)
-	return gulp
-		.src(['lib/**/*.test.js'])
-		.pipe(casper({
-			verbose: true
-		})); 
-});
-
 /* Launch the app or parts of it */
 gulp.task('start', function() {
   // Start the app
   nodemon({
   	script: 'app.js'
   });
-});
-
-gulp.task('server', function() {
-	// Just start the server
-	nodemon({
-		script: 'lib/server/server.js',
-		ext: '.js',
-		ignore: ['*.spec.js', '*.test.js']
-	});
 });
