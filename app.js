6f69e27f825a71fd199a8bf8376ebd0576dc125f
Brad Dickason <brad.dickason@oculus.com>
2017-05-14 22:13:28 -0700

    Removing a bunch of console logs

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ var options = {
     events: eventbus // Pass in our eventbus so we can call events directly
 };
 
-var Overlays = require('twitch-overlay');
-// var Overlays = require('../twitch-overlay');
+// var Overlays = require('twitch-overlay');
+var Overlays = require('../twitch-overlay');
 Overlays.start(options);
 
 // Initialize individual overlays
