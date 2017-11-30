8a9f7e9c7716ae4e26b7e43f9acd9c1e32ed57e3
Brad Dickason <brad.dickason@oculus.com>
2017-11-29 22:32:01 -0800

    Music app now runs react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'lib/music/static/js');
+var APP_DIR = path.resolve(__dirname, 'lib/music/client');
+
+var config = {
+  entry: APP_DIR + '/music-client.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
