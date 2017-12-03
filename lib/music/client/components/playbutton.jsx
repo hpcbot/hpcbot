f8258d582444b5212d8ba117b7431ba566252d3c
Brad Dickason <brad.dickason@oculus.com>
2017-12-02 19:35:10 -0800

    Fully replaced previous jquery hacked music player

--- lib/music/client/components/playbutton.jsx
+++ lib/music/client/components/playbutton.jsx
@@ -6,7 +6,6 @@ import {render} from 'react-dom';
 class PlayButton extends React.Component {
   constructor(props) {
     super(props);
-    // this.onToggle = this.onToggle.bind(this);
   }
 
   render() {
@@ -16,15 +15,6 @@ class PlayButton extends React.Component {
       return <button onClick={this.props.onToggle}>❚❚</button>
     }
   }
-
-  //
-  // play() {
-  //   this.setState({playing: true});
-  // }
-  //
-  // pause() {
-  //   this.setState({playing: false});
-  // }
 }
 
 export default PlayButton
--- lib/music/client/components/player.jsx
+++ lib/music/client/components/player.jsx
@@ -9,10 +9,17 @@ class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        player: null
+        player: null,
+        title: null,
+        duration: null,
+        minutes: null,
+        seconds: null,
+        progress: null
     };
-    // this.play = this.play.bind(this);
+
     this._onReady = this._onReady.bind(this);
+    this._onChangeVideo = this._onChangeVideo.bind(this);
+    this.updateMetadata = this.updateMetadata.bind(this);
   }
 
   render() {
@@ -41,22 +48,74 @@ class Player extends React.Component {
       }
     }
 
-    return(<YouTube
-        videoId={this.props.videoId}
-        opts={options}
-        onReady={this._onReady}
-      />);
+    return(<div id='player'>
+              <YouTube
+                videoId={this.props.videoId}
+                opts={options}
+                onReady={this._onReady}
+                onChangeVideo={this._onChangeVideo}
+              />
+              <p>{this.state.title}</p>
+              <p>Time: {this.state.minutes}:{this.state.seconds} ({this.state.progress})</p>
+          </div>);
   }
 
   _onReady(event) {
-    // access to player in all event handlers via event.target
-    this.setState({player: event.target});
+    this.setState({
+      player: event.target,   // Keep track of the player so we can access it
+    });
+
+    // Set a timer to poll the api for metadata changes
+    setInterval(this.updateMetadata, 60);
+  }
+
+  _onChangeVideo(event) {
+    this.setState({
+      title: this.state.player.getVideoData().title
+    });
   }
 
   _onEnd(event) {
     // Notify parent when the song ends
     console.log('end');
     // this.props.onToggle();
+  }
+
+  updateMetadata() {
+    this.setState({
+      title: this.state.player.getVideoData().title,
+      duration: this.state.player.getVideoData().duration,
+      minutes: this.parseMinutes(this.state.player.getCurrentTime()),
+      seconds: this.parseSeconds(this.state.player.getCurrentTime()),
+      progress: this.parsePercentage(this.state.player.getCurrentTime(), this.state.player.getDuration())
+    })
+  }
+  /* Metadata utility functions */
+
+  parseMinutes(time) {
+    var minutes = Math.round(time/60);
+    return(minutes);
+  }
+
+  parseSeconds(time) {
+    var seconds = Math.round(time % 60);
+
+    // Fix some weirdness with the youtube timestamp
+		if(seconds == 60) {
+			// For some reason 60 shows up on the seconds count
+			seconds = '00';
+		} else if(seconds >= 0 && seconds < 10) {
+			// Prepend zero
+			seconds = 0 + seconds.toString();
+		}
+
+		return seconds;
+  }
+
+  parsePercentage(time, duration) {
+    var percentage = Math.round(time / duration * 100);
+    percentage = percentage.toString() + '%';
+    return(percentage);
   }
 }
 
