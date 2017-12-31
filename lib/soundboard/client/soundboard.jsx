9a4684aff52c6f52ec17862de8159abbbcaa86fd
Brad Dickason <brad.dickason@oculus.com>
2017-12-30 17:57:15 -0800

    Started conversion to react

---
+++ lib/soundboard/client/soundboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class Soundboard extends React.Component {
+  render() {
+ 
+  }
+}
+
+render(<Soundboard/>, document.getElementById('soundboard'));
